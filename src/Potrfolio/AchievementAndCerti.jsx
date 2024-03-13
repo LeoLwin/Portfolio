e140aa66dcfc46e4f91473555a847f03745859d6
KHL <phoekaung.3819@gmail.com>
2024-03-13 11:29:23 +0630

    responsive for Achievemnet

--- src/Potrfolio/AchievementAndCerti.jsx
+++ src/Potrfolio/AchievementAndCerti.jsx
@@ -3,7 +3,7 @@ import znova1 from "../assets/znova1.png";
 const AchievementAndCerti = () => {
   return (
     <div className="achieve border border-info rounded-4 text-center">
-      <img src={znova1} alt="" width={700} className="mt-3 rounded-4 mb-2 border border-black" />
+      <img src={znova1} alt="" width={700} className="mt-3 rounded-4 mb-2 border border-black col-md-12" />
       <h4>Full Stack Web Development One-on One Class</h4>
       <a href="https://znova.one/courses/1/Full-Stack-Web-Development-One-on-One-Class">
         https://znova.one/
